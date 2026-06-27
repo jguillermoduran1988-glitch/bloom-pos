36fa392e82728975453952235f259c3b83e82ab8
jguillermoduran1988-glitch <jguillermoduran1988@gmail.com>
2026-06-26 19:07:29 -0500

    chore: bump cache to v26

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="bloom-v25";
+const CACHE="bloom-v26";
 const PUSH_LATEST_URL="https://bloom-push.jguillermoduran1988.workers.dev/push/latest?store=bloom";
 const SHELL=["./index.html","./app.js","./config.js","./push-client.js","./manifest.json","./colombia.js","./logo-bloom.svg","./icon-192.png","./icon-512.png"];
 
